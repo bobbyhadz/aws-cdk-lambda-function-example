80915208be0b07a4893021b960d89363add81497
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-01 07:52:59 +0300

    added infra

---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,25 @@
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as cdk from '@aws-cdk/core';
+import * as path from 'path';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 👇 lambda function definition
+    const lambdaFunction = new lambda.Function(this, 'lambda-function', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      handler: 'index.main',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src/my-lambda')),
+      environment: {
+        REGION: cdk.Stack.of(this).region,
+        AVAILABILITY_ZONES: JSON.stringify(
+          cdk.Stack.of(this).availabilityZones,
+        ),
+      },
+      reservedConcurrentExecutions: 1,
+    });
+  }
+}
